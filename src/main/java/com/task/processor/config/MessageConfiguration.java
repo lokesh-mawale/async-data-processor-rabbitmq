22ee51aecdda447a45be3c9d0d37055921a0e894
LokeshMawale <lokesh.mawale87@gmail.com>
2025-05-21 08:27:57 +0530

    Correction in README.md Changes for Pulling docker image

--- src/main/java/com/task/processor/config/MessageConfiguration.java
+++ src/main/java/com/task/processor/config/MessageConfiguration.java
@@ -9,10 +9,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 public class MessageConfiguration {
